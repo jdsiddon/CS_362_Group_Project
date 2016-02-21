ced84b27a319d19d38b16577b6833aeb75331e53
Andrew Pierno <andrewpierno@gmail.com>
2016-02-21 09:28:54 -0800

    initial test is valid (programming tests)

--- URLValidator/src/UrlValidatorTest.java
+++ URLValidator/src/UrlValidatorTest.java
@@ -36,38 +36,77 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-   
-   
+
+
    public void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 	   System.out.println(urlVal.isValid("http://www.amazon.com"));
-	   
-	   
+
+
    }
-   
-   
+
+
    public void testYourFirstPartition()
    {
-	   
+
    }
-   
+
    public void testYourSecondPartition(){
-	   
+
    }
-   
-   
+
+
    public void testIsValid()
    {
-	   for(int i = 0;i<10000;i++)
+     int length = 10000;        // loop counter
+     int bugs = 0;              // bug counter
+     int r = 0;                 // second loop counter
+
+     /* Failed Urls */
+     String[] failed = new String[length]; // will hold the failed urls.
+
+     /* Url Pieces */
+     String[] schemes    = { "http://", "", "ftp://", "h3t://" }; // all valid schemes
+     String[] authority  = { "www.google.com", "google.com", "go.com", "go.au", "0.0.0.0", "255.255.255.255", "255.com", "go.cc" }; // all valid authorities
+     String[] ports      = { ":80", ":90", ":100", ":3000", ":8888" }; // all valid ports
+     String[] path       = { "/test", "/t123", "/$23", "/test/", "/test/next" }; // all valid paths
+     String[] options    = { "/test1", "/t123", "/$23", "/test/", "/$23/file" }; // all valid path options
+     String[] queries    = { "?action=view", "?action=edit&mode=down", "", "?set=true&bob=dylan&you=true" }; // all valid queries
+
+	   for(int i = 0; i<length; i++)
 	   {
-		   
-	   }
-   }
-   
+      int scheme_index    = (int) (Math.random() * 3);
+      int authority_index = (int) (Math.random() * 7);
+      int ports_index     = (int) (Math.random() * 4);
+      int path_index      = (int) (Math.random() * 4);
+      int options_index   = (int) (Math.random() * 4);
+      int queries_index   = (int) (Math.random() * 3);
+
+      /* Create a random string made up of valid url pieces */
+      String url = schemes[scheme_index] + authority[authority_index] + ports[ports_index] + path[path_index] + queries[queries_index];
+      UrlValidator validator =  new UrlValidator();
+      boolean test = validator.isValid(url);
+
+      if ( test == false ) {
+        bugs++;           // increase number of bugs
+        failed[r] = url;  // save the failed url
+        r++;              // increment the counter
+      }
+     } //end for loop
+
+     /* Print out number of bugs and failed urls */
+     System.out.println("Tests found " + bugs + " bugs. \n");
+     System.out.println("Failed urls: \n");
+     for ( int i=0; i< failed.length; i++) {
+       if ( failed[i] != null ) System.out.println( i + " : " + failed[i] + "\n");
+     }
+
+   } // end test is valid
+
    public void testAnyOtherUnitTest()
    {
-	   
+
    }
    /**
     * Create set of tests by taking the testUrlXXX arrays and
@@ -75,6 +114,6 @@ public class UrlValidatorTest extends TestCase {
     *
     * @param testObjects Used to create a url.
     */
-   
+
 
 }
