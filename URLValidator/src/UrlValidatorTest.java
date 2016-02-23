78f1a31f9736144214e5c4fd237795be36a6692b
mstryff <stryffem@onid.oregonstate.edu>
2016-02-22 17:55:31 -0800

    Added Bad Url Programming Tests
    Added a second loop to the programming tests function which creates bad
    urls and tests to see if they are rejected as expected.

--- URLValidator/src/UrlValidatorTest.java
+++ URLValidator/src/UrlValidatorTest.java
@@ -59,13 +59,14 @@ public class UrlValidatorTest extends TestCase {
 
    public void testIsValid()
    {
-     int length = 10000;        // loop counter
+     int length = 500;         // loop counter
      int bugs = 0;              // bug counter
      int r = 0;                 // second loop counter
 
      /* Failed Urls */
      String[] failed = new String[length]; // will hold the failed urls.
 
+     /* Test Valid Urls */
      /* Url Pieces */
      String[] schemes    = { "http://", "", "ftp://", "h3t://" }; // all valid schemes
      String[] authority  = { "www.google.com", "google.com", "go.com", "go.au", "0.0.0.0", "255.255.255.255", "255.com", "go.cc" }; // all valid authorities
@@ -74,7 +75,7 @@ public class UrlValidatorTest extends TestCase {
      String[] options    = { "/test1", "/t123", "/$23", "/test/", "/$23/file" }; // all valid path options
      String[] queries    = { "?action=view", "?action=edit&mode=down", "", "?set=true&bob=dylan&you=true" }; // all valid queries
 
-	   for(int i = 0; i<length; i++)
+     for(int i = 0; i<length; i++)
 	   {
       int scheme_index    = (int) (Math.random() * 3);
       int authority_index = (int) (Math.random() * 7);
@@ -94,12 +95,87 @@ public class UrlValidatorTest extends TestCase {
         r++;              // increment the counter
       }
      } //end for loop
-
+     	 
      /* Print out number of bugs and failed urls */
-     System.out.println("Tests found " + bugs + " bugs. \n");
+     System.out.println("Valid Url Tests found " + bugs + " bugs. \n");
      System.out.println("Failed urls: \n");
      for ( int i=0; i< failed.length; i++) {
        if ( failed[i] != null ) System.out.println( i + " : " + failed[i] + "\n");
+     }
+     
+     System.out.println("Valid Url Tests Complete");
+	 /* Test Invalid Urls */
+     bugs = 0;
+     r = 0;
+     /* Invalid Passed Urls */
+     String[] passed = new String[length]; // will hold the invalid urls which pass
+     
+     /* Invalid Pieces */
+     String[] badSchemes 	= { "3ht://", "http:", "http:/", "://", "http/" };
+     String[] badAuthority 	= { "www.google.1com", "256.256.256.256", "16.16.16.16.16", "55.55.55", ".255.255.255.255", "192.68.102.10.", "abc", "abc.123" };
+     String[] badPorts 		= { ":-1", ":8888a", ":70000", "22000" };
+     String[] badPath 		= { "/..", "/../", "/path/to//file", "//#" };
+     String[] badOptions 	= { "/../", "/#", "/test//file" };
+     
+     /* Create a random string made up of valid url pieces and one invalid piece */
+     for(int i = 0; i < length; i++)
+	 {
+    	 /* Create Valid Url */
+         int scheme_index    = (int) (Math.random() * 3);
+         int authority_index = (int) (Math.random() * 7);
+         int ports_index     = (int) (Math.random() * 4);
+         int path_index      = (int) (Math.random() * 4);
+         int options_index   = (int) (Math.random() * 4);
+         int queries_index   = (int) (Math.random() * 3);
+         
+         
+         /* Replace One Piece with an Invalid Piece */
+         /* Reassign Indices to Invalid Pieces */
+		 int bad_scheme_index 		= (int) (Math.random() * 4);
+		 int bad_authority_index 	= (int) (Math.random() * 7);
+		 int bad_ports_index     	= (int) (Math.random() * 3);
+		 int bad_path_index      	= (int) (Math.random() * 3);
+		 int bad_options_index   	= (int) (Math.random() * 2);
+		 int bad_queries_index   	= (int) (Math.random() * 3);
+	     
+	     /* Choose the Piece to Replace */
+	     int replace_piece		= (int) (Math.random() * 3);
+	     
+	     /* Create a random string made up of valid url pieces and on invalid piece */
+	     String url;
+	     if ( replace_piece == 0 )		//Replace scheme
+	     {
+	         url = badSchemes[bad_scheme_index] + authority[authority_index] + ports[ports_index] + path[path_index] + queries[queries_index];
+	     }
+	     else if( replace_piece == 1 )	//Replace authority
+	     {
+	    	 url = schemes[scheme_index] + badAuthority[bad_authority_index] + ports[ports_index] + path[path_index] + queries[queries_index];
+	     }
+	     else if( replace_piece == 2)	//Replace port
+	     {
+	    	 url = schemes[scheme_index] + authority[authority_index] + badPorts[bad_ports_index] + path[path_index] + queries[queries_index];
+	     }
+	     else							//Replace path
+	     {
+	    	 url = schemes[scheme_index] + authority[authority_index] + ports[ports_index] + badPath[bad_path_index] + queries[queries_index];
+	     }
+	     
+	     //System.out.println(i + " : " + url + "\n");
+         
+         UrlValidator validator =  new UrlValidator();
+         boolean test = validator.isValid(url);
+         if ( test == true ) {
+             bugs++;           // increase number of bugs
+             passed[r] = url;  // save the failed url
+             r++;              // increment the counter
+         }
+	 }	//end for loop
+
+     /* Print out number of bugs and improperly passed urls */
+     System.out.println("Invalid Url Tests found " + bugs + " bugs. \n");
+     System.out.println("Incorrectly Passed Urls: \n");
+     for ( int i=0; i< failed.length; i++) {
+       if ( passed[i] != null ) System.out.println( i + " : " + passed[i] + "\n");
      }
 
    } // end test is valid
